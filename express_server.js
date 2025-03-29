a3b316c868753f209242cfe1546c5bbacf8f1c10
Jason Chen <windson7@gmail.com>
2025-03-28 23:49:53 -0700

    added new lines to handle new urls and post new urls

--- express_server.js
+++ express_server.js
@@ -3,6 +3,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  const charPool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const poolLength = charPool.length;
+
+  let output = '';
+
+  for (let outputDigit = 0; outputDigit < 6; outputDigit++) {
+    charIndex = Math.floor(Math.random() * poolLength)
+    output += charPool[charIndex];
+  }
+
+  return output;
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +46,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
