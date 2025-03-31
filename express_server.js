ecf43ed9db344128b2e19f35faff0538fbae5ee2
Jason Chen <windson7@gmail.com>
2025-03-30 17:55:51 -0700

    added register.ejs for registration page with POST in /register and GET in server.js

--- express_server.js
+++ express_server.js
@@ -62,12 +62,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { 
-      username: req.cookies["username"],
-      id: req.params.id, 
-      longURL: urlDatabase[req.params.id] 
-    };
-    res.render("urls_show", templateVars);
+  const templateVars = { 
+    username: req.cookies["username"],
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] 
+  };
+  res.render("urls_show", templateVars);
+});
+
+
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("register",  templateVars);
 });
 
 app.post("/urls", (req, res) => {
